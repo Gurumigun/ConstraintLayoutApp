5cac4b9d3c1f5a6278f0830e7a2248f33eac402c
kwonikyoung <gurumi132@gmail.com>
2017-03-21 15:08:32 +0900

    1. ConstrainLayout 추가중

--- app/src/main/java/com/gurumi/testconstraintlayout/MainActivity.java
+++ app/src/main/java/com/gurumi/testconstraintlayout/MainActivity.java
@@ -1,13 +1,28 @@
 package com.gurumi.testconstraintlayout;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.gurumi.testconstraintlayout.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Log.d("KIY" , "onCreate()");
+        if (mBinding != null) {
+            Log.d("KIY", "DataBinding OK! ") ;
+        } else {
+            Log.e("KIY", "DataBinding is NULL! ") ;
+        }
+
     }
 }
