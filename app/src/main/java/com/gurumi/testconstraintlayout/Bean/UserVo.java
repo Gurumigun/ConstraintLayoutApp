4827ed71b7295bea0993f6d7415a5e553789446e
kwonikyoung <gurumi132@gmail.com>
2017-03-22 10:54:28 +0900

    1. Dagger2 , Retrofit2, Rxjava+RxAndroid Gradle 추가

---
+++ app/src/main/java/com/gurumi/testconstraintlayout/Bean/UserVo.java
@@ -0,0 +1,12 @@
+package com.gurumi.testconstraintlayout.Bean;
+
+/**
+ * Created by 1100488 on 2017. 3. 21..
+ */
+
+public class UserVo {
+
+    public String name;
+    public String phone;
+    public String memo;
+}
--- app/src/main/java/com/gurumi/testconstraintlayout/MainActivity.java
+++ app/src/main/java/com/gurumi/testconstraintlayout/MainActivity.java
@@ -2,11 +2,17 @@ package com.gurumi.testconstraintlayout;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.util.Log;
+import android.view.View;
 
+import com.gurumi.testconstraintlayout.Bean.UserVo;
 import com.gurumi.testconstraintlayout.databinding.ActivityMainBinding;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +30,25 @@ public class MainActivity extends AppCompatActivity {
             Log.e("KIY", "DataBinding is NULL! ") ;
         }
 
+        mBinding.btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setConstraintlayoutStateChange();
+                UserVo user = new UserVo();
+                user.name = "User Name : " + new Random().nextInt(100);
+                user.phone = "Phone : " + new Random().nextInt();
+                user.memo = "memo : " + user.name + " // " + user.phone;
+
+                mBinding.setUser(user);
+            }
+        });
+    }
+
+    public void setConstraintlayoutStateChange(){
+        if (mBinding.constraintLayout.getVisibility() == View.VISIBLE) {
+            mBinding.constraintLayout.setVisibility(View.GONE);
+        } else {
+            mBinding.constraintLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
